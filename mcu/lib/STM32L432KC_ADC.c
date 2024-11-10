1974d5849d28a85d8cc6a75a5c6432f5d357f077
ZhianZ <zzhou@g.hmc.edu>
2024-11-10 14:07:17 -0800

    add mcu lib

---
+++ mcu/lib/STM32L432KC_ADC.c
@@ -0,0 +1,12 @@
+// STM32L432KC_ADC.h
+// Source code for ADC functions
+
+#include "STM32L432KC_ADC.h"
+
+void configureADC() {
+    // Enable GPIO Clock
+    gpioEnable(GPIO_PORT_A)
+    
+
+    
+}
