da27cd0ece297cee9766a8d405215691e466f9cf
ZhianZ <zzhou@g.hmc.edu>
2024-11-13 21:43:12 -0800

    update DMA

---
+++ mcu/segger/STM32L432KC_ADC.h
@@ -0,0 +1,21 @@
+// STM32L432KC_ADC.h
+// Header for ADC functions
+
+#ifndef STM32L4_ADC_H
+#define STM32L4_ADC_H
+
+#include <stdint.h>
+#include <stm32l432xx.h>
+#include "STM32L432KC_GPIO.h"
+
+#define PIN_INPUT PB0
+
+///////////////////////////////////////////////////////////////////////////////
+// Function prototypes
+///////////////////////////////////////////////////////////////////////////////
+
+void ms_delay(int ms);
+void configureADC(void);
+uint8_t readADC(void);
+
+#endif
