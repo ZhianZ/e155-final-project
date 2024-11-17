2814be6b3d9e2ecdd5835b156c5762808174611a
Jackson <jacksonphilion@gmail.com>
2024-11-17 14:43:20 -0800

    Working Demo

---
+++ demo/segger_demo/Output/Debug/Obj/project_demo/SEGGER_THUMB_Startup.d
@@ -0,0 +1,2 @@
+Output/Debug/Obj/project_demo/SEGGER_THUMB_Startup.o: \
+ C:\Users\jphilion\Desktop\e155-final-project\demo\segger_demo\SEGGER_THUMB_Startup.s
---
+++ demo/src/main.c
@@ -0,0 +1,47 @@
+/*********************************************************************
+*                    SEGGER Microcontroller GmbH                     *
+*                        The Embedded Experts                        *
+**********************************************************************
+
+-------------------------- END-OF-HEADER -----------------------------
+
+File    : main.c
+Purpose : Generic application start
+
+*/
+
+#include <stdio.h>
+#include "../lib/STM32L432KC.h"
+
+/*********************************************************************
+*
+*       main()
+*
+*  Function description
+*   Application entry point.
+*/
+int main(void) {
+    configureFlash();
+    configureClock();
+
+    configureADC(); // Configure ADC
+    
+    // Initialize timer 2 for delay
+    RCC->APB1ENR1 |= RCC_APB1ENR1_TIM2EN;
+    initTIM(TIM2);
+
+    while (1) {
+        // Read ADC value
+        uint16_t adcValue = readADC();
+
+        // Print ADC value
+        printf("ADC Value: %d\n", adcValue);
+
+        // Add delay
+        delay_millis(TIM2, 500);
+    }
+
+    return 0;
+}
+
+/*************************** End of file ****************************/
